d1e9439a4635db700dcbafc9b105b29908c6711b
mani6027 <manisaraha@gmail.com>
2020-08-12 20:00:48 +0530

    Add commands module to integrate with heroku-postgres

--- app/config.py
+++ app/config.py
@@ -8,20 +8,8 @@ LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
 LOGGER = logging.getLogger(__name__)
 
-
-# Database details
-DATABASE_HOST = os.getenv('DATABASE_HOST')
-DATABASE_NAME = os.getenv('DATABASE_NAME')
-DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
-DATABASE_PORT = int(os.getenv('DATABASE_PORT', 5432))
-DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-
 # Secretkey
 SECRET_KEY = b'_5#y2L"F4Q8z\n\xec]/'
 
-
 # Databse uri
-DATABASE_URI = f'postgresql+psycopg2://{DATABASE_USERNAME}:' \
-               f'{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
+DATABASE_URI = os.getenv('DATABASE_URL')
--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
-from sqlalchemy.dialects.postgresql import ENUM, UUID
-from flask_login import UserMixin
-from sqlalchemy.orm import relationship
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from uuid import uuid4
+
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy.dialects.postgresql import ENUM, UUID
+from sqlalchemy.orm import relationship
 
 DB = SQLAlchemy()
 
@@ -15,9 +15,9 @@ class User(UserMixin, DB.Model):
     __tablename__ = 'account'
 
     id = Column(UUID(as_uuid=True), primary_key=True)
-    email = Column(String, nullable=False)
-    username = Column(String, nullable=False)
-    password = Column(String, nullable=False)
+    email = Column(String(50), nullable=False)
+    username = Column(String(50), nullable=False)
+    password = Column(String(200), nullable=False)
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime)
 
@@ -27,8 +27,8 @@ class Todo(DB.Model):
     __tablename__ = 'todo'
 
     id = Column(UUID(as_uuid=True), primary_key=True)
-    task_name = Column(String, nullable=False)
+    task_name = Column(Text, nullable=False)
     status = Column(TASK_STATUS, nullable=False)
-    email = Column(Text, nullable=False)
+    email = Column(String(50), nullable=False)
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime)
